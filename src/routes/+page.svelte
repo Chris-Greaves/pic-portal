6c3bfbda9dd753090eca53ca303e6ab24a836b45
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2025-09-30 03:09:55 +0100

    Inital Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+import Uppy from '@uppy/core'
+import {
+  Dropzone,
+  FilesGrid,
+  FilesList,
+  UploadButton,
+  UppyContextProvider,
+} from '@uppy/svelte'
+import Tus from '@uppy/tus'
+import '@uppy/svelte/css/style.css'
+
+const uppy = new Uppy()
+  .use(Tus, {
+    //endpoint: 'https://tusd.tusdemo.net/files/',
+    endpoint: 'http://localhost:8080/files/',
+  })
+
+let dialogRef: HTMLDialogElement
+</script>
+
+<UppyContextProvider {uppy}>
+  <main class="p-5 max-w-xl mx-auto">
+    <h1 class="text-4xl font-bold my-4">Welcome to the pic-portal!</h1>
+
+    <p>Select your files and click upload</p>
+
+    <Dropzone />
+    <UploadButton />
+
+    <dialog
+      bind:this={dialogRef}
+      class="backdrop:bg-gray-500/50 rounded-lg shadow-xl p-0 fixed inset-0 m-auto"
+    >
+    </dialog>
+
+    <article>
+      <h2 class="text-2xl my-4">Files</h2>
+      <FilesList />
+    </article>
+
+    <article>
+      <h2 class="text-2xl my-4">Thumbnails</h2>
+      <FilesGrid columns={3} />
+    </article>
+  </main>
+</UppyContextProvider>
