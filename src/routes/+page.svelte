a91a012392de4a1b78711f8ac4c901c42ecd160d
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2025-10-08 03:23:10 +0100

    Move to using the built-in dashboard for uploading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
 import Uppy from '@uppy/core'
-import {
-  Dropzone,
-  FilesList,
-  FilesGrid,
-  UploadButton,
-  UppyContextProvider,
-} from '@uppy/svelte'
+import { type DashboardOptions } from '@uppy/dashboard'
+import Dashboard from '@uppy/svelte/dashboard'
 import Tus from '@uppy/tus'
 import Compressor from '@uppy/compressor';
 
 import '@uppy/svelte/css/style.css'
+import '@uppy/dashboard/css/style.min.css';
 
 const uppy = new Uppy()
   .use(Tus, {
@@ -19,32 +15,42 @@ const uppy = new Uppy()
   })
   .use(Compressor)
 
-  let dialogRef: HTMLDialogElement
+uppy.setOptions({
+	restrictions: { allowedFileTypes: ["image/*", "video/*"] },
+	autoProceed: true,
+});
+
+const dashboardOptions = {
+  note: "Images won't be shared on Social Media without the permission of all seen in the photo",
+  proudlyDisplayPoweredByUppy: false
+} satisfies Partial<DashboardOptions<any, any>>
+
+let nameValue: string = "";
+
+function updateMetadata() {
+  uppy.setMeta({uploader_name: nameValue})
+}
 </script>
 
-<UppyContextProvider {uppy}>
-  <main class="p-5 max-w-xl mx-auto">
-    <h1 class="text-4xl font-bold my-4">Welcome to the pic-portal!</h1>
+<main class="p-5 max-w-xl mx-auto">
+  <h1 class="text-4xl text-center font-bold my-4">Thank you for attending!</h1>
 
-    <p>Select your files and click upload</p>
+  <h3 class="text-l text-center my-4">If you wish to share your photos with the bride and groom, please provide your name, and upload the photos using the section below:</h3>
 
-    <Dropzone />
-    <UploadButton />
+  <div>
+      <!-- <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label> -->
+      <input placeholder="Your name..." bind:value={nameValue} on:change={updateMetadata} type="text" id="name" class="name-field text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-4" required />
+  </div>
+  <Dashboard uppy = {uppy} props = {dashboardOptions}/>
+</main>
 
-    <dialog
-      bind:this={dialogRef}
-      class="backdrop:bg-gray-500/50 rounded-lg shadow-xl p-0 fixed inset-0 m-auto"
-    >
-    </dialog>
-
-    <article>
-      <h2 class="text-2xl my-4">Files</h2>
-      <FilesList />
-    </article>
-
-    <article>
-      <h2 class="text-2xl my-4">Thumbnails</h2>
-      <FilesGrid columns={3} />
-    </article>
-  </main>
-</UppyContextProvider>
+<style>
+  body {
+    font-family: ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  }
+  .name-field {
+    background-color: #f4f4f4;
+    color: #000000;
+    font-weight: 400;
+  }
+</style>
