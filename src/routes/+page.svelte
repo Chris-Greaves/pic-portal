88326df6033eafaffacf168a25a6457b9c0ff8cb
Chris-Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2025-10-14 01:18:00 +0100

    Require name before showing modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 import Uppy from '@uppy/core'
 import { type DashboardOptions } from '@uppy/dashboard'
 import Dashboard from '@uppy/svelte/dashboard'
+import DashboardModal from '@uppy/svelte/dashboard-modal'
 import Tus from '@uppy/tus'
 import Compressor from '@uppy/compressor';
 
@@ -27,22 +28,25 @@ const dashboardOptions = {
 } satisfies Partial<DashboardOptions<any, any>>
 
 let nameValue: string = "";
+let allowUpload: boolean = false;
 
-function updateMetadata() {
+function nameFieldChanged() {
   uppy.setMeta({uploaded_by: nameValue})
+  allowUpload = nameValue !== ""
 }
 </script>
 
 <main class="p-5 max-w-xl mx-auto">
   <h1 class="text-4xl text-center font-bold my-4">Thank you for attending!</h1>
 
-  <h3 class="text-l text-center my-4">If you wish to share your photos with the bride and groom, please provide your name, and upload the photos using the section below:</h3>
+  <h3 class="text-l text-center my-4">If you wish to share your photos with the bride and groom, please provide your name below, and a modal should pop up allowing you to upload as many photos as you like</h3>
 
   <div>
       <!-- <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label> -->
-      <input placeholder="Your name..." bind:value={nameValue} on:change={updateMetadata} type="text" id="name" class="name-field text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-4" required />
+      <input placeholder="Your name..." bind:value={nameValue} on:change={nameFieldChanged} type="text" id="name" class="name-field text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-4" required />
   </div>
-  <Dashboard uppy = {uppy} props = {dashboardOptions}/>
+  <!-- <Dashboard uppy = {uppy} props = {dashboardOptions}/> -->
+  <DashboardModal uppy = {uppy} props = {dashboardOptions} open = {allowUpload}/>
 </main>
 
 <style>
